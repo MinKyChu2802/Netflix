b7499638aef26167e197531db37e98cbc00f8242
QuynCM <quycm@edsolabs.com>
2023-04-19 14:04:53 +0700

    add connect mongodb, create prisma

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,30 +1,43 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
 import Input from '@/components/Input';
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 
 const Auth = () => {
   const [value, setValue] = useState({ email: '', username: '', password: '' })
+  const [variant, setVariant] = useState('login')
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
+  }, [])
 
   const onChange = (e: any) => {
-    console.log('Quy', e.target.name)
     setValue({ ...value, [e.target.name]: e.target.value })
   }
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
       <div className='bg-black w-full h-full lg:bg-opacity-50 '>
-        <nav className='pz-12 py-5'> <img src="/images/logo.png" alt="" className='h-12' /></nav>
+        <nav className='px-12 py-5'> <img src="/images/logo.png" alt="" className='h-12' /></nav>
         <div className='flex justify-center'>
           <div className='bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md w-full'>
             <h2 className='text-white text-4xl mb-8 font-semibold'>
-              Sign in
+              {variant === 'login' ? 'Sign in' : 'Register'}
             </h2>
             <div className='flex flex-col gap-4'>
-              <Input id='username' label='Username' onChange={onChange} value={value.username} />
+              {variant === 'register' && <Input id='username' label='Username' onChange={onChange} value={value.username} />}
               <Input id='email' label='Email' onChange={onChange} value={value.email} />
-              <Input id='password' label='Password' onChange={onChange} value={value.password} />
+              <Input id='password' label='Password' onChange={onChange} value={value.password} type='password' />
             </div>
+
+            <button className='bg-red-600 text-white 
+            rounded-md w-full mt-10 hover:bg-red-700
+            py-3
+             transition'> {variant === 'login' ? 'Sign in' : 'Register'}</button>
+            <p className='text-neutral-500 mt-12'>
+              {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
+              <span className='text-white ml-1 hover:underline cursor-pointer' onClick={toggleVariant}>{variant === 'login' ? 'Create an account' : 'Login'}</span>
+            </p>
           </div>
         </div>
       </div>
