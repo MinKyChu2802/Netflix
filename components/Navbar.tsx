7fece29db81eff716ea250c4b3cafbbf30e8e139
QuynCM <quycm@edsolabs.com>
2023-04-20 17:49:09 +0700

    update component Billbroad, Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,63 +1,77 @@
-/* eslint-disable @next/next/no-img-element */
-import React, { FC, useCallback, useState } from 'react'
-import NavbarItem from './NavbarItem'
-import { BsChevronDown, BsSearch, BsBell } from 'react-icons/bs'
-import MobileMenu from './MobileMenu'
-import AccountMenu from './AccountMenu'
+import React, { useCallback, useEffect, useState } from 'react';
+import { BellIcon, MagnifyingGlassIcon, ChevronDownIcon } from '@heroicons/react/24/outline';
 
-const Navbar: FC = () => {
-  const [showMobileMenu, setShowMobileMenu] = useState<boolean>(false)
+import AccountMenu from '@/components/AccountMenu';
+import MobileMenu from '@/components/MobileMenu';
+import NavbarItem from '@/components/NavbarItem';
 
-  const toggleMenu = useCallback(() => {
-    setShowMobileMenu((current) => !current)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+const TOP_OFFSET = 66;
+
+const Navbar = () => {
+  const [showAccountMenu, setShowAccountMenu] = useState(false);
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showBackground, setShowBackground] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      console.log(window.scrollY)
+      if (window.scrollY >= TOP_OFFSET) {
+        setShowBackground(true)
+      } else {
+        setShowBackground(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, []);
+
+  const toggleAccountMenu = useCallback(() => {
+    setShowAccountMenu((current) => !current);
+  }, []);
+
+  const toggleMobileMenu = useCallback(() => {
+    setShowMobileMenu((current) => !current);
+  }, []);
 
   return (
-    <nav className='w-full fixed z-40'>
-      <div className="px-4 md:px-16 
-      py-6 flex flex-row items-center 
-      transition duration-500 
-      bg-zinc-900 bg-opacity-90">
-        <img className='h-4 lg:h-7' src="/images/logo.png" alt="" />
+    <nav className="w-full fixed z-40">
+      <div className={`px-4 md:px-16 py-6 flex flex-row items-center transition duration-500 ${showBackground ? 'bg-zinc-900 bg-opacity-90' : ''}`}>
+        <img src="/images/logo.png" className="h-4 lg:h-7" alt="Logo" />
         <div className="flex-row ml-8 gap-7 hidden lg:flex">
-          <NavbarItem label='Home' />
-          <NavbarItem label='Series' />
-          <NavbarItem label='Films' />
-          <NavbarItem label='New & Popular' />
-          <NavbarItem label='My List' />
-          <NavbarItem label='Browse by languages' />
+          <NavbarItem label="Home" active />
+          <NavbarItem label="Series" />
+          <NavbarItem label="Films" />
+          <NavbarItem label="New & Popular" />
+          <NavbarItem label="My List" />
+          <NavbarItem label="Browse by Languages" />
         </div>
-
-        <div onClick={toggleMenu} className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
+        <div onClick={toggleMobileMenu} className="lg:hidden flex flex-row items-center gap-2 ml-8 cursor-pointer relative">
           <p className="text-white text-sm">Browse</p>
-          <BsChevronDown className='text-white transition' />
+          <ChevronDownIcon className={`w-4 text-white fill-white transition ${showMobileMenu ? 'rotate-180' : 'rotate-0'}`} />
           <MobileMenu visible={showMobileMenu} />
         </div>
-
         <div className="flex flex-row ml-auto gap-7 items-center">
-          <div className="text-gray-200 hover:text-gray-300 cursor-poiter transition">
-            <BsSearch />
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+            <MagnifyingGlassIcon className="w-6" />
           </div>
-
-          <div className="text-gray-200 hover:text-gray-300 cursor-poiter transition">
-            <BsBell />
+          <div className="text-gray-200 hover:text-gray-300 cursor-pointer transition">
+            <BellIcon className="w-6" />
           </div>
-
-          <div className="flex flex-row items-center gap-2 cursor-pointer relative">
+          <div onClick={toggleAccountMenu} className="flex flex-row items-center gap-2 cursor-pointer relative">
             <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
               <img src="/images/default-blue.png" alt="" />
             </div>
-            <BsChevronDown className='text-white transition' />
-            <AccountMenu />
+            <ChevronDownIcon className={`w-4 text-white fill-white transition ${showAccountMenu ? 'rotate-180' : 'rotate-0'}`} />
+            <AccountMenu visible={showAccountMenu} />
           </div>
         </div>
-
-
-
       </div>
     </nav>
   )
 }
 
-export default Navbar
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,23 @@
-import Navbar from '@/components/Navbar'
-import useCurrentUser from '@/hooks/useCurrentUser'
-import { NextPageContext } from 'next'
-import { getSession } from 'next-auth/react'
+import React from 'react';
+import { NextPageContext } from 'next';
+import { getSession } from 'next-auth/react';
+
+import Navbar from '@/components/Navbar';
+import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import InfoModal from '@/components/InfoModal';
+import useMovieList from '@/hooks/useMovieList';
+import useFavorites from '@/hooks/useFavorites';
+import useInfoModalStore from '@/hooks/useInfoModalStore';
 
 export async function getServerSideProps(context: NextPageContext) {
-  const session = await getSession(context)
+  const session = await getSession(context);
 
   if (!session) {
     return {
       redirect: {
         destination: '/auth',
-        permanent: false
+        permanent: false,
       }
     }
   }
@@ -20,14 +27,22 @@ export async function getServerSideProps(context: NextPageContext) {
   }
 }
 
-export default function Home() {
-  const { data: user } = useCurrentUser()
-
-
+const Home = () => {
+  const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
+  const { isOpen, closeModal } = useInfoModalStore();
 
   return (
     <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
+      <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
+      </div>
     </>
   )
 }
+
+export default Home;
